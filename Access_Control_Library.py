8635aad950f2f79aad5af81e2e5b089c6704ea30
Nightwingbeyond <30642762+Nightwingbeyond@users.noreply.github.com>
2019-10-02 14:26:18 -0400

    init

---
+++ Access_Control_Library.py
@@ -0,0 +1,16 @@
+
+#Object to store the name and password of a user 
+class User:
+    def __init__(self,name,password):
+        self.name = name
+        self.password = password
+
+    
+
+# p1 = User("myname", "mypassword")
+
+# print(p1.name)
+# print(p1.password)
+
+class Group:
+    def __init__(self,)
